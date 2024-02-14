0cc06b6d657dd477fb21a849e3fe5d64e861abff
shubhpanchal <shubhpanchal412@gmail.com>
2024-02-14 19:26:35 +0530

    App Completed

---
+++ app.py
@@ -0,0 +1,33 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+import streamlit as st
+
+genai.configure(api_key='AIzaSyDtLzNrhwHgf5yUqJErKagJDU8S8Lxg2jM')
+model = genai.GenerativeModel('gemini-pro')
+
+
+def getGeminiresponse(user_input):
+    response = model.generate_content(user_input)
+    return response.text
+
+st.set_page_config(page_title = "Know your thing😎" ,
+                page_icon = "🤖",
+                layout = "centered",
+                initial_sidebar_state = "collapsed")
+  
+
+st.header("Know your thing😎")
+
+user_input = st.text_input("What are you curious about? 🤔💭")
+
+submit = st.button("Find Out")
+
+
+if submit:
+    st.write(getGeminiresponse(user_input=user_input))
